d5413fb820aba309518ddfd91c94d0822d3d983b
v3rb0se <punkc957@gmail.com>
2023-04-09 05:54:29 +0500

    Add necessary components

--- app/src/main/java/com/stfu/social/MainActivity.java
+++ app/src/main/java/com/stfu/social/MainActivity.java
@@ -1,10 +1,38 @@
 package com.stfu.social;
-import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.HapticFeedbackConstants;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.materialswitch.MaterialSwitch;
 import com.topjohnwu.superuser.Shell;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String WHATSAPP_PACKAGE = "com.whatsapp";
+    private final String SNAPCHAT_PACKAGE = "com.snapchat.android";
+    private final String INSTAGRAM_PACKAGE = "com.instagram.android";
+    private final String REDDIT_PACKAGE = "com.reddit.frontpage";
+    private final String TELEGRAM_PACKAGE = "org.telegram.messenger";
+    private final String FACEBOOK_PACKAGE = "com.facebook.katana";
+    private final String DISCORD_PACKAGE = "com.discord";
+
+    MaterialSwitch
+            Instagram,
+            Snapchat,
+            Whatsapp,
+            Reddit,
+            Telegram,
+            Facebook,
+            Discord;
+    Button button;
 
     static {
         // Set settings before the main shell can be created
@@ -14,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
                 .setTimeout(10)
         );
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
