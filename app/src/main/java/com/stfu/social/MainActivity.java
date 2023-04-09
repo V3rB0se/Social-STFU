3e07492fda79cfa26d7c5bb01ba46a573ce300b5
v3rb0se <punkc957@gmail.com>
2023-04-09 05:52:26 +0500

    Add libsu & bump material 1.5.0 to 1.8.0

--- app/src/main/java/com/stfu/social/MainActivity.java
+++ app/src/main/java/com/stfu/social/MainActivity.java
@@ -1,14 +1,27 @@
 package com.stfu.social;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import com.topjohnwu.superuser.Shell;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    static {
+        // Set settings before the main shell can be created
+        Shell.enableVerboseLogging = BuildConfig.DEBUG;
+        Shell.setDefaultBuilder(Shell.Builder.create()
+                .setFlags(Shell.FLAG_REDIRECT_STDERR)
+                .setTimeout(10)
+        );
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
