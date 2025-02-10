a9e1f37af4c6e8e5a4b134bdd17fd67edb3f1b5b
v3rb0se <punkc957@gmail.com>
2025-02-11 02:57:19 +0500

    Refactor

--- app/src/main/java/com/stfu/social/MainActivity.java
+++ app/src/main/java/com/stfu/social/MainActivity.java
@@ -1,4 +1,5 @@
 package com.stfu.social;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -13,24 +14,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.materialswitch.MaterialSwitch;
 import com.topjohnwu.superuser.Shell;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private final String WHATSAPP_PACKAGE = "com.whatsapp";
-    private final String SNAPCHAT_PACKAGE = "com.snapchat.android";
-    private final String INSTAGRAM_PACKAGE = "com.instagram.android";
-    private final String REDDIT_PACKAGE = "com.reddit.frontpage";
-    private final String TELEGRAM_PACKAGE = "org.telegram.messenger";
-    private final String FACEBOOK_PACKAGE = "com.facebook.katana";
-    private final String DISCORD_PACKAGE = "com.discord";
+    private static final String WHATSAPP_PACKAGE = "com.whatsapp";
+    private static final String SNAPCHAT_PACKAGE = "com.snapchat.android";
+    private static final String INSTAGRAM_PACKAGE = "com.instagram.android";
+    private static final String REDDIT_PACKAGE = "com.reddit.frontpage";
+    private static final String TELEGRAM_PACKAGE = "org.telegram.messenger";
+    private static final String FACEBOOK_PACKAGE = "com.facebook.katana";
+    private static final String DISCORD_PACKAGE = "com.discord";
 
-    MaterialSwitch
-            Instagram,
-            Snapchat,
-            Whatsapp,
-            Reddit,
-            Telegram,
-            Facebook,
-            Discord;
+    MaterialSwitch Instagram, Snapchat, Whatsapp, Reddit, Telegram, Facebook, Discord;
     Button button;
 
     static {
@@ -40,152 +37,103 @@ public class MainActivity extends AppCompatActivity {
         );
     }
 
+    private final List<AppInfo> appInfoList = Arrays.asList(
+            new AppInfo(WHATSAPP_PACKAGE, R.id.whatsapp_switch, R.id.whatsapp_desc),
+            new AppInfo(SNAPCHAT_PACKAGE, R.id.snapchat_switch, R.id.snapchat_desc),
+            new AppInfo(INSTAGRAM_PACKAGE, R.id.instagram_switch, R.id.instagram_desc),
+            new AppInfo(REDDIT_PACKAGE, R.id.reddit_switch, R.id.reddit_desc),
+            new AppInfo(TELEGRAM_PACKAGE, R.id.telegram_switch, R.id.telegram_desc),
+            new AppInfo(FACEBOOK_PACKAGE, R.id.facebook_switch, R.id.facebook_desc),
+            new AppInfo(DISCORD_PACKAGE, R.id.discord_switch, R.id.discord_desc)
+    );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Instagram = findViewById(R.id.instagram_switch);
-        Snapchat = findViewById(R.id.snapchat_switch);
-        Whatsapp = findViewById(R.id.whatsapp_switch);
-        Reddit = findViewById(R.id.reddit_switch);
-        Telegram = findViewById(R.id.telegram_switch);
-        Discord = findViewById(R.id.discord_switch);
-        Facebook = findViewById(R.id.facebook_switch);
-
         button = findViewById(R.id.Exit);
-        checkApps(this,  Instagram,
-                Snapchat,
-                Reddit,
-                Telegram,
-                Whatsapp,
-                Discord,
-                Facebook);
+
+        checkAndSetupApps(this);
+        setupSwitchListeners();
+        button.setOnClickListener(view -> finish());
+    }
 
 
-        button.setOnClickListener((view -> finish())
-        );
+    @SuppressLint("SetTextI18n")
+    private void checkAndSetupApps(Context context) {
+        PackageManager pm = context.getPackageManager();
 
-        Reddit.setOnCheckedChangeListener((materialSwitch, isChecked) -> toggleApp(REDDIT_PACKAGE, R.id.reddit_desc, isChecked, Reddit));
+        for (AppInfo appInfo : appInfoList) {
+            boolean isAppInstalled = isAppInstalled(pm, appInfo.packageName);
+            MaterialSwitch appSwitch = findViewById(appInfo.switchId);
+            appSwitch.setEnabled(isAppInstalled);
+            checkStatus(appInfo.packageName, appSwitch);
+            ChangeStateMessage(appInfo.descId, isAppInstalled, appSwitch);
+        }
+    }
 
-        Telegram.setOnCheckedChangeListener((materialSwitch, isChecked) -> toggleApp(TELEGRAM_PACKAGE, R.id.telegram_desc, isChecked, Telegram));
 
-        Instagram.setOnCheckedChangeListener((materialSwitch, isChecked) -> toggleApp(INSTAGRAM_PACKAGE, R.id.instagram_desc, isChecked, Instagram));
+    private boolean isAppInstalled(PackageManager pm, String packageName) {
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
+    }
 
-        Snapchat.setOnCheckedChangeListener((materialSwitch, isChecked) -> toggleApp(SNAPCHAT_PACKAGE, R.id.snapchat_desc, isChecked, Snapchat));
 
-        Whatsapp.setOnCheckedChangeListener((materialSwitch, isChecked) -> toggleApp(WHATSAPP_PACKAGE, R.id.whatsapp_desc, isChecked, Whatsapp));
-
-        Facebook.setOnCheckedChangeListener((materialSwitch, isChecked) -> toggleApp(FACEBOOK_PACKAGE, R.id.facebook_desc, isChecked, Facebook));
-
-        Discord.setOnCheckedChangeListener((materialSwitch, isChecked) -> toggleApp(DISCORD_PACKAGE, R.id.discord_desc, isChecked, Discord));
-
+    private void setupSwitchListeners() {
+        for (AppInfo appInfo : appInfoList) {
+            MaterialSwitch appSwitch = findViewById(appInfo.switchId);
+            appSwitch.setOnCheckedChangeListener((switchView, isChecked) -> toggleApp(appInfo.packageName, appInfo.descId, isChecked, switchView));
+        }
     }
 
 
     @SuppressLint("SetTextI18n")
     public void toggleApp(String packageName, int id, boolean isChecked, View view) {
         TextView tv = findViewById(id);
+        String action = isChecked ? "disable" : "enable";
 
-        if (isChecked) {
-            Shell.cmd("pm disable " + packageName).exec();
-            view.performHapticFeedback(HapticFeedbackConstants.REJECT);
-            tv.setText("Application is Disabled");
-            Toast.makeText(this, packageName + " Disabled", Toast.LENGTH_LONG).show();
-        } else {
-            Shell.cmd("pm enable " + packageName).exec();
-            view.performHapticFeedback(HapticFeedbackConstants.REJECT);
-            Toast.makeText(this, packageName + " Enabled", Toast.LENGTH_LONG).show();
-            tv.setText("Application is Enabled");
-        }
+        Shell.cmd("pm " + action + " " + packageName).exec();
+        view.performHapticFeedback(HapticFeedbackConstants.REJECT);
+
+        tv.setText("Application is " + (isChecked ? "Disabled" : "Enabled"));
+        Toast.makeText(this, packageName + " " + (isChecked ? "Disabled" : "Enabled"), Toast.LENGTH_LONG).show();
     }
 
-    public void checkStatus(String appName, MaterialSwitch switcher){
+
+    public void checkStatus(String packageName, MaterialSwitch switcher) {
         Shell.Result result = Shell.cmd("pm list packages -d").exec();
         String output = TextUtils.join("\n", result.getOut());
-        boolean isDisabled = output.contains(appName);
+        boolean isDisabled = output.contains(packageName);
         switcher.setChecked(isDisabled);
-
-    }
-    @SuppressLint("SetTextI18n")
-    public void checkApps(Context context, MaterialSwitch Instagram, MaterialSwitch Snapchat, MaterialSwitch Reddit, MaterialSwitch Telegram, MaterialSwitch Whatsapp, MaterialSwitch Discord, MaterialSwitch Facebook){
-
-        String[] appPackageNames = {INSTAGRAM_PACKAGE, WHATSAPP_PACKAGE, DISCORD_PACKAGE, REDDIT_PACKAGE, TELEGRAM_PACKAGE, SNAPCHAT_PACKAGE, FACEBOOK_PACKAGE};
-
-        for (String packageName : appPackageNames) {
-            PackageManager pm = context.getPackageManager();
-            boolean isAppInstalled;
-
-            try {
-                pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
-                isAppInstalled = true;
-            } catch (PackageManager.NameNotFoundException e) {
-                isAppInstalled = false;
-            }
-
-            switch (packageName) {
-                case INSTAGRAM_PACKAGE:
-                    checkStatus(INSTAGRAM_PACKAGE, Instagram);
-                    Instagram.setEnabled(isAppInstalled);
-                    ChangeStateMessage(R.id.instagram_desc, isAppInstalled, Instagram);
-
-                    break;
-                case TELEGRAM_PACKAGE:
-                    checkStatus(TELEGRAM_PACKAGE, Telegram);
-                    Telegram.setEnabled(isAppInstalled);
-                    ChangeStateMessage(R.id.telegram_desc, isAppInstalled, Telegram);
-
-                    break;
-                case REDDIT_PACKAGE:
-                    checkStatus(REDDIT_PACKAGE, Reddit);
-                    Reddit.setEnabled(isAppInstalled);
-                    ChangeStateMessage(R.id.reddit_desc, isAppInstalled, Reddit);
-
-
-                    break;
-                case WHATSAPP_PACKAGE:
-                    checkStatus(WHATSAPP_PACKAGE, Whatsapp);
-                    Whatsapp.setEnabled(isAppInstalled);
-                    ChangeStateMessage(R.id.whatsapp_desc, isAppInstalled, Whatsapp);
-
-                    break;
-                case SNAPCHAT_PACKAGE:
-                    checkStatus(SNAPCHAT_PACKAGE, Snapchat);
-                    Snapchat.setEnabled(isAppInstalled);
-                    ChangeStateMessage(R.id.snapchat_desc, isAppInstalled, Snapchat);
-
-                    break;
-                case DISCORD_PACKAGE:
-                    checkStatus(DISCORD_PACKAGE, Discord);
-                    Discord.setEnabled(isAppInstalled);
-                    ChangeStateMessage(R.id.discord_desc, isAppInstalled, Discord);
-
-                    break;
-                case FACEBOOK_PACKAGE:
-                    checkStatus(FACEBOOK_PACKAGE, Facebook);
-                    Facebook.setEnabled(isAppInstalled);
-                    ChangeStateMessage(R.id.facebook_desc, isAppInstalled, Facebook);
-                    break;
-            }
-        }
-
     }
 
 
     @SuppressLint("SetTextI18n")
-    public void ChangeStateMessage(int tid, Boolean isAppInstalled, MaterialSwitch switcher){
-
+    public void ChangeStateMessage(int tid, Boolean isAppInstalled, MaterialSwitch switcher) {
         TextView tv = findViewById(tid);
 
-        if(!isAppInstalled){
+        if (!isAppInstalled) {
             tv.setText("Application Not Found");
-        }
-        if(switcher.isChecked()){
-            tv.setText("Application is Disabled");
-        }
-        if(!switcher.isChecked() && isAppInstalled){
-            tv.setText("Application is Enabled");
+        } else {
+            tv.setText(switcher.isChecked() ? "Application is Disabled" : "Application is Enabled");
         }
     }
 
+
+    public static class AppInfo {
+        String packageName;
+        int switchId;
+        int descId;
+
+        public AppInfo(String packageName, int switchId, int descId) {
+            this.packageName = packageName;
+            this.switchId = switchId;
+            this.descId = descId;
+        }
+    }
 }
